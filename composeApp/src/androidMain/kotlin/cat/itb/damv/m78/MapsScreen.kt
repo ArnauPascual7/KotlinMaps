5148bb07c34f1d2827a3fbb5f81b0ca8f8a87954
arnau.pascual.7e8 <arnau.pascual.7e8@itb.cat>
2025-05-06 10:45:57 +0200

    Update

--- composeApp/src/androidMain/kotlin/cat/itb/damv/m78/MapsScreen.kt
+++ composeApp/src/androidMain/kotlin/cat/itb/damv/m78/MapsScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -23,28 +24,39 @@ import com.google.maps.android.compose.rememberCameraPositionState
 @Composable
 fun MapsScreen(){
     val markers = remember { mutableStateListOf<LatLng>() }
+    val cameraPositionState: CameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(LatLng(40.41,-3.70), 5f)
+    }
 
-    GoogleMap(
-        googleMapOptionsFactory = {
-            GoogleMapOptions().mapId("DEMO_MAP_ID")
-        },
-        onMapClick = { latLng ->
-            markers.add(latLng)
-        }
-    ) {
-        AdvancedMarker(
-            state = MarkerState(position = LatLng(-34.0, 151.0)),
-            title = "Marker in Sydney"
-        )
-        AdvancedMarker(
-            state = MarkerState(position = LatLng(35.66, 139.6)),
-            title = "Marker in Tokyo"
-        )
-        markers.forEach { latLng ->
+    Box(Modifier.fillMaxSize()) {
+        GoogleMap(
+            googleMapOptionsFactory = {
+                GoogleMapOptions().mapId("DEMO_MAP_ID")
+            },
+            onMapClick = { latLng ->
+                markers.add(latLng)
+            },
+            cameraPositionState = cameraPositionState
+        ) {
             AdvancedMarker(
-                state = MarkerState(position = latLng),
-                title = "Nuevo marcador"
+                state = MarkerState(position = LatLng(-34.0, 151.0)),
+                title = "Marker in Sydney"
             )
+            AdvancedMarker(
+                state = MarkerState(position = LatLng(35.66, 139.6)),
+                title = "Marker in Tokyo"
+            )
+            markers.forEach { latLng ->
+                AdvancedMarker(
+                    state = MarkerState(position = latLng),
+                    title = "Nuevo marcador"
+                )
+            }
         }
+        Button(
+            onClick = {
+                cameraPositionState.position = CameraPosition.fromLatLngZoom(LatLng(40.41,-3.70), 5f)
+            }
+        ) { }
     }
 }
